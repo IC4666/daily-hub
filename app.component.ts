d0f8ea7881482855d3c9676295fffa953187e1f9
IC4666 <imonchowdhury4666@gmail.com>
2025-11-03 21:13:36 +0600

    added new liquid glass effect and added scrolling animation using lenis

--- app.component.ts
+++ app.component.ts
@@ -1,7 +1,9 @@
-import { signal, Component } from '@angular/core';
+import { signal, Component, AfterViewInit } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { routeAnimations } from './components/route-animations/route-animations.component';
+import Lenis from 'lenis';
+
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,21 @@ import { routeAnimations } from './components/route-animations/route-animations.
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = signal('DailyHub');
   prepareRoute = (outlet: RouterOutlet) => outlet?.activatedRouteData?.['animation'];
+
+  // for smooth scrolling effect
+   ngAfterViewInit() {
+    const lenis = new Lenis({
+      duration: 2.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }
 }
